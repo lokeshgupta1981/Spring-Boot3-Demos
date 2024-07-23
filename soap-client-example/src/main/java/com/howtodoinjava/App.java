968d1f11b721d32757c01313daf3a3d5414e0314
lokeshgupta1981 <howtodoinjava@gmail.com>
2024-07-23 13:59:57 +0530

    Refactor

--- soap-client-example/src/main/java/com/howtodoinjava/App.java
+++ soap-client-example/src/main/java/com/howtodoinjava/App.java
@@ -11,24 +11,24 @@ import com.example.howtodoinjava.schemas.school.StudentDetailsResponse;
 @SpringBootApplication
 public class App {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 
-    @Bean
-    CommandLineRunner lookup(SOAPConnector soapConnector) {
-        return args -> {
-            String name = "Sajal";//Default Name
-            if(args.length>0){
-                name = args[0];
-            }
-            StudentDetailsRequest request = new StudentDetailsRequest();
-            request.setName(name);
-            StudentDetailsResponse response =(StudentDetailsResponse) soapConnector.callWebService("http://localhost:8080/service/student-details", request);
-            System.out.println("Got Response As below ========= : ");
-            System.out.println("Name : "+response.getStudent().getName());
-            System.out.println("Standard : "+response.getStudent().getStandard());
-            System.out.println("Address : "+response.getStudent().getAddress());
-        };
-    }
+  @Bean
+  CommandLineRunner lookup(SOAPConnector soapConnector) {
+    return args -> {
+      String name = "Lokesh"; //Default Name
+      if (args.length > 0) {
+        name = args[0];
+      }
+      StudentDetailsRequest request = new StudentDetailsRequest();
+      request.setName(name);
+      StudentDetailsResponse response = (StudentDetailsResponse) soapConnector.callWebService("http://localhost:8080/service/student-details", request);
+      System.out.println("Got Response As below ========= : ");
+      System.out.println("Name : " + response.getStudent().getName());
+      System.out.println("Standard : " + response.getStudent().getStandard());
+      System.out.println("Address : " + response.getStudent().getAddress());
+    };
+  }
 }
